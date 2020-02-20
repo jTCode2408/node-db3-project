5ff965323683f71bf5729524438e905d8dd96713
jTCode2408 <jasmine.terry91@gmail.com>
2020-02-19 19:00:32 -0500

    Get, GetbyID, Delete schemes tested, correct

---
+++ schemes/scheme-model.js
@@ -0,0 +1,46 @@
+//helpers for db
+const db = require('../data/seeds/db-config');
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+};
+
+//find ALL
+function find() {
+    return db('schemes');
+}
+//find BYID
+function findById(id) {
+    return db('schemes')
+        .where({ id })
+        .first();
+}
+//FINDSTEPS(find steps for specific scheme)
+function findSteps(id) {
+    return db('steps')
+        .join('schemes', 'schemes.id', 'scheme.scheme_name')
+        .select('steps.step_number', 'steps.instructions', 'scheme.id', 'scheme.scheme_name' )
+    .where({'scheme_id': id})
+}
+//ADD
+function add(scheme) {
+    return db('schemes').insert(scheme, 'id')
+        // .then(([id]) => get(id));
+}
+//UPDATE
+function update(id, changes) {
+    return db('schemes')
+        .where({ id })
+        .update({ changes })
+}
+//REMOVE
+function remove(id) {
+    return db('schemes')
+        .where({ id })
+        .del();
+}
